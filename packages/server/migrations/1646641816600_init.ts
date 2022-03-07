2a4e9dbff5051ab33ccf5915c117ed8eca6f9d28
Kory Porter <kory.porter@hotmail.com>
2022-03-07 21:34:09 +1100

    add initial migration

---
+++ packages/server/migrations/1646641816600_init.ts
@@ -0,0 +1,153 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { MigrationBuilder, ColumnDefinitions } from 'node-pg-migrate';
+
+export const shorthands: ColumnDefinitions | undefined = undefined;
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+  pgm.createType('sensor_type', ['moisture']);
+  pgm.createType('solenoid_state', ['on', 'off', 'forced_on', 'forced_off']);
+
+  pgm.createTable('config', {
+    id: {
+      type: 'uuid',
+      primaryKey: true,
+    },
+    soil_sensor_update_interval_ms: {
+      type: 'integer',
+      notNull: true,
+    },
+    created_at: {
+      type: 'timestamp',
+      notNull: true,
+      default: 'now',
+    },
+  });
+
+  pgm.createTable('controller', {
+    id: {
+      type: 'uuid',
+      primaryKey: true,
+    },
+    friendly_name: {
+      type: 'varchar(255)',
+    },
+    ip_address: {
+      type: 'varchar(15)',
+    },
+    online: {
+      type: 'boolean',
+      notNull: true,
+      default: false,
+    },
+    last_boot: {
+      type: 'timestamp',
+    },
+    config_id: {
+      type: 'uuid',
+      notNull: true,
+      references: 'config',
+      onDelete: 'RESTRICT',
+    },
+  });
+
+  pgm.createTable('zone', {
+    id: {
+      type: 'uuid',
+      primaryKey: true,
+    },
+    name: {
+      type: 'varchar(50)',
+    },
+    controller_id: {
+      type: 'uuid',
+      references: 'controller',
+      onDelete: 'SET NULL',
+    },
+  });
+
+  pgm.createTable('sensor', {
+    id: {
+      type: 'uuid',
+      primaryKey: true,
+    },
+    type: {
+      type: 'sensor_type',
+      notNull: true,
+    },
+    zone_id: {
+      type: 'uuid',
+      notNull: true,
+      references: 'zone',
+      onDelete: 'CASCADE',
+    },
+  });
+
+  pgm.createTable('sensor_reading', {
+    id: {
+      type: 'uuid',
+      primaryKey: true,
+    },
+    sensor_id: {
+      type: 'uuid',
+      notNull: true,
+      references: 'sensor',
+      onDelete: 'CASCADE',
+    },
+    created_at: {
+      type: 'timestamp',
+      notNull: true,
+      default: 'now',
+    },
+    reading: {
+      type: 'double precision',
+      notNull: true,
+    },
+  });
+
+  pgm.createTable('solenoid', {
+    id: {
+      type: 'uuid',
+      primaryKey: true,
+    },
+    zone_id: {
+      type: 'uuid',
+      notNull: true,
+      references: 'zone',
+      onDelete: 'CASCADE',
+    },
+    state: {
+      type: 'solenoid_state',
+      notNull: true,
+      default: 'off',
+    },
+  });
+
+  pgm.createTable('irrigation_job', {
+    id: {
+      type: 'uuid',
+      primaryKey: true,
+    },
+    zone_id: {
+      type: 'uuid',
+      notNull: true,
+      references: 'zone',
+      onDelete: 'CASCADE',
+    },
+    active: {
+      type: 'boolean',
+      notNull: true,
+    },
+    start: {
+      type: 'timestamp',
+      notNull: true,
+    },
+    end: {
+      type: 'timestamp',
+      notNull: true,
+    },
+  });
+}
+
+export async function down(pgm: MigrationBuilder): Promise<void> {
+  //
+}
